a60df124865bb3d8d234b62883c82532a69b5089
Gilmarfilho2003 <89646761+Gilmarfilho2003@users.noreply.github.com>
2021-09-14 20:13:04 -0300

    Create senha.py

---
+++ senha.py
@@ -0,0 +1,30 @@
+print('''
+ 
+                     __         
+   ________  ____  / /_  ____ _
+  / ___/ _ \/ __ \/ __ \/ __ `/
+ (__  )  __/ / / / / / / /_/ / 
+/____/\___/_/ /_/_/ /_/\__,_/  
+                               
+
+
+              ''')
+
+               deseja gerar sua senha 
+                 1- completo
+    
+    if do == '1':
+
+from random import choice
+import string
+
+tamanho = 10
+list
+valores = string.ascii_lowercase + string.digits
+senha = ''
+for i in range(tamanho):
+ senha += choice(valores)
+
+
+print (senha) 
+
