622a63099b34691ecf50f2a3ea3cda76a59a8362
Gilmarfilho2003 <89646761+Gilmarfilho2003@users.noreply.github.com>
2021-09-16 14:39:02 -0300

    Update senha2.py

--- senha2.py
+++ senha2.py
@@ -1,9 +1,10 @@
 import string,random
 ram =[]
-for i in range(10000):
+for i in range(10000000000000000000000000000000000):
 	senha = ''
-	for i in range(10):
-		senha+= random.choice(string.digits+' ')
+	for i in range(6):
+		senha+= random.choice(string.ascii_letters+' ')
 	if not senha in ram:
 		ram.append(senha)
 		print(senha)
+ 
